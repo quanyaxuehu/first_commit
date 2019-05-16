1d75b101aa01068ac3b2c397593fa65ee1cceb96
qyw <2442530079@qq.com>
2019-05-16 20:14:14 +0800

    this is a try2

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 import argparse
 import sys
+
+##hello word
+##what are you doing 
+
+parser = argparse.ArgumentParser(
+    description='sum the integers at the command line')
+parser.add_argument(
+    'integers', metavar='int', nargs='+', type=int,
+    help='an integer to be summed')
+parser.add_argument(
+    '--log', default=sys.stdout, type=argparse.FileType('w'),
+    help='the file where the sum should be written')
+args = parser.parse_args()
+args.log.write('%s' % sum(args.integerss))
+args.log.close()
+
+###I don't know what is this
 parser = argparse.ArgumentParser(
     description='sum the integers at the command line')
 parser.add_argument(
